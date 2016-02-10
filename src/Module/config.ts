32adddd6a4152d9b546ea47393ca123e3d91a8e6
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-10 12:01:39 +0100

    Aggiunte brevi descrizioni

--- src/Module/config.ts
+++ src/Module/config.ts
@@ -1,8 +1,15 @@
 /**
+ * Dichiarazione variabili globali all'interno del modulo Module.
+ * Preferito al posto di un file di configurazione in quando non sono opzioni
+ * negoziabili durante l'uso del programma.
+ *
  * @author @korut94
  */
 
 module Module {
+    // Directory dei plugins
     export const _PATH : string = "plugins/";
+    // Regex in cui si preleva il nome l'autore del plugin e il nome del
+    // plugin
     export const _EXPR_GIT : RegExp = /:(.*)\.git$/;
 }

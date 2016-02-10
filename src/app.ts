7bae73df8b227110873bfc58b02f9dfe70eff025
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-10 17:18:24 +0100

    Esempio di chiamata ad un modulo dell'utente

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,11 @@
 /// <reference path="Module/dl_plugin.ts" />
 /// <reference path="Module/rm_plugin.ts" />
 /// <reference path="Module/up_plugin.ts" />
+/// <reference path="Module/run_plugin.ts" />
 
-console.log( "Download plugin..." );
-var code : number = Module.downloadPlugin(
-    "git@github.com:korut94/LinQedIn.git"
-);
-console.log( "Download completato!" );
-
-
-console.log( "Update plugin..." );
-var code : number = Module.updatePlugin(
-    "git@github.com:korut94/LinQedIn.git"
+var [ result, code ] = Module.runModule(
+    "git@github.com:korut94/Test.git",
+    "Test"
 );
 
-console.log( `Terminato con stato ${code}` );
+console.log( `Terminato con stato ${code} e risultato ${result}` );

2ef0ec0b6ce7cfc828bca94a2a33e2aea4cd6cba
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-10 13:11:05 +0100

    Inserito il modulo con i codici d'errore

--- src/Module/config.ts
+++ src/Module/config.ts
@@ -7,9 +7,18 @@
  */
 
 module Module {
-    // Directory dei plugins
+    //Directory dei plugins
     export const _PATH : string = "plugins/";
-    // Regex in cui si preleva il nome l'autore del plugin e il nome del
-    // plugin
+    //Regex in cui si preleva il nome l'autore del plugin e il nome del
+    //plugin
     export const _EXPR_GIT : RegExp = /:(.*)\.git$/;
+    //Modulo presente i codici d'uscita dei moduli
+    export module Code{
+        //Uscita con successo
+        export const _SUCCESS : number = 0;
+        //Errore generale
+        export const _GENERAL_ERROR : number = 1
+        //Argomento non valido
+        export const _INVALID_ARG : number = 128;
+    }
 }

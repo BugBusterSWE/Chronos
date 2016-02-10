33a455303159306b581871b0a577fc0e77293291
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-10 12:02:25 +0100

    Dimostrazione download e rimozione di un plugin

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 /**
+ * Script di partenze per l'esecuzione di Chronos
+ *
  * Created by amantova on 09/02/16.
  */
 
-/// <reference path="Command/CommandI.ts" />
-/// <reference path="Command/CommandSync.ts" />
+/// <reference path="Module/dl_plugin.ts" />
+/// <reference path="Module/rm_plugin.ts" />
 
-var command : Command.CommandI = new Command.CommandSync(
-    'git',
-    ['clone','git@github.com:korut94/LinQedIn.git']
+console.log( "Download plugin..." );
+var code : number = Module.downloadPlugin(
+    "git@github.com:korut94/LinQedIn.git"
 );
+console.log( "Download completato!" );
 
-console.log( command.exec() );
+console.log( "Remove plugin..." );
+var code : number = Module.removePlugin(
+    "git@github.com:korut94/LinQedIn.git"
+);
+console.log( `Terminato con stato ${code}` );

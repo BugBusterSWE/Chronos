bfe6261cf88d23106d4d8337bbe59530673ccb43
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-21 13:42:50 +0100

    Script to inizialize global variables

---
+++ src/gui/init.js
@@ -0,0 +1,13 @@
+/**
+ * Creo le variabili globali necessarie per il corretto funzionamento di
+ * Chronos.
+ *
+ * Created by amantova on 21/02/16.
+ */
+
+// Ogni plugin dovra' specificare in questa variabile il suo nome, in tal
+// maniera si potra' eseguire la procedura d'esecuzione dei moduli senza
+// specificare il nome del plugin, accorciando la stringa del paramentro.
+// E' compito del modulo di trasmissione al plugin manager process come viene
+// definito la forma abbreviata da usare.
+var __plugin = undefined;

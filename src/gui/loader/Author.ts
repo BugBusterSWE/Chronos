b14d4a471258d3136bdb256d51eefc53fd8f4e34
Matteo Di Pirro <matteodipirro@gmail.com>
2016-02-24 17:42:33 +0100

    Added plugins list and integration functions

--- src/gui/loader/Author.ts
+++ src/gui/loader/Author.ts
@@ -1,3 +1,52 @@
 /**
  * Created by matteo on 24/02/16.
+ * @author Matteo Di Pirro
  */
+
+/**
+ * An author of one, or more, plugins.
+ */
+class Author {
+    /**
+     * The name of the author
+     */
+    private name: string;
+
+    /**
+     * An array which contains all the plugins written by the author.
+     */
+    private plugins: string[];
+
+    /**
+     * Constructs an author
+     * @param authorName The name of the author.
+     */
+    constructor(authorName: string) {
+        this.name = authorName;
+        this.plugins = [];
+    }
+
+    /**
+     * This method allows to add a plugin to this author.
+     * @param pluginName The name of the plugin.
+     */
+    addPlugin(pluginName: string): void {
+        this.plugins.push(pluginName);
+    }
+
+    /**
+     * This method returns the name of the author.
+     * @returns {string} The name of the author.
+     */
+    getName(): string{
+        return this.name;
+    }
+
+    /**
+     * This method returns an array which contains the plugins written by the author.
+     * @returns {string[]} Array of plugin names.
+     */
+    getPlugins(): string[] {
+        return this.plugins;
+    }
+}
--- src/gui/loader/loader.js
+++ src/gui/loader/loader.js
@@ -1,9 +1,45 @@
 /**
  * Created by matteo on 24/02/16.
+ * @author Matteo Di Pirro
  */
-
-declare var require : any;
-
+/**
+ * An author of one, or more, plugins.
+ */
+var Author = (function () {
+    /**
+     * Constructs an author
+     * @param authorName The name of the author.
+     */
+    function Author(authorName) {
+        this.name = authorName;
+        this.plugins = [];
+    }
+    /**
+     * This method allows to add a plugin to this author.
+     * @param pluginName The name of the plugin.
+     */
+    Author.prototype.addPlugin = function (pluginName) {
+        this.plugins.push(pluginName);
+    };
+    /**
+     * This method returns the name of the author.
+     * @returns {string} The name of the author.
+     */
+    Author.prototype.getName = function () {
+        return this.name;
+    };
+    /**
+     * This method returns an array which contains the plugins written by the author.
+     * @returns {string[]} Array of plugin names.
+     */
+    Author.prototype.getPlugins = function () {
+        return this.plugins;
+    };
+    return Author;
+})();
+/**
+ * Created by matteo on 24/02/16.
+ */
 /**
  * Given an html file which contains the main GUI of Chronos, this function load the specific GUI of a plugin and put
  * it into a <div id='plugin'></div>. This div must be present in the main GUI. The specific GUI must be into a file
@@ -19,22 +55,21 @@ function initGUI(author, plugin) {
         document.getElementById('plugin').innerHTML = ((err) ? err : data);
     });
 }
-
 /**
  * List all the authors and, for each author, the plugins.
  */
 function listPlugins() {
     var fs = require('fs');
-    var root : string = './plugins/'
-    var files: string[] = fs.readdirSync(root);
-    var authors : Author[] = [];
-    for(var i = 0; i < files.length; i++) {
-        var author: Author = new Author(files[i]);
-        var plugins: string[] = fs.readdirSync(root + files[i]);
+    var root = './plugins/';
+    var files = fs.readdirSync(root);
+    var authors = [];
+    for (var i = 0; i < files.length; i++) {
+        var author = new Author(files[i]);
+        var plugins = fs.readdirSync(root + files[i]);
         for (var j = 0; j < plugins.length; j++) {
             var file = plugins[j];
             var filePath = root + author.getName() + '/' + file;
-            var stat: any = fs.statSync(filePath);
+            var stat = fs.statSync(filePath);
             if (stat.isDirectory()) {
                 author.addPlugin(file);
                 console.log(author.getPlugins());
@@ -78,19 +113,18 @@ function listPlugins() {
         }
     });*/
 }
-
 /**
  * Creates a definition list (<dl>) in function of the passed array. The <dt> tags are author names, the <dd> tags are
  * the plugins of that actor.
  * @param authors An array which contains the authors of the installed plugins.
  * @returns {string} A definition list with the specified properties (see above).
  */
-function createHTMLList(authors: Author[]): string {
-    var dl: string = '<dl>';
-    for (var i: number = 0; i < authors.length; i++) {
+function createHTMLList(authors) {
+    var dl = '<dl>';
+    for (var i = 0; i < authors.length; i++) {
         dl += '<dt>' + authors[i].getName() + '</dt>';
-        var plugins: string[] = authors[i].getPlugins();
-        for (var j: number = 0; j < plugins.length; j++) {
+        var plugins = authors[i].getPlugins();
+        for (var j = 0; j < plugins.length; j++) {
             dl += '<dd>' + plugins[j] + '</dd>';
         }
     }
@@ -98,3 +132,4 @@ function createHTMLList(authors: Author[]): string {
     document.getElementById('plugin').innerHTML = dl;
     return dl;
 }
+//# sourceMappingURL=loader.js.map

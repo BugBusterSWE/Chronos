94093ddd95c0d3c33879e18e71665346b7454997
korut94 <andreamnt94@gmail.com>
2016-02-29 16:44:30 +0100

    Extract author and plugin name both https and ssh addresses

--- src/module/config.ts
+++ src/module/config.ts
@@ -11,14 +11,15 @@ module Module {
     export const _PATH : string = "plugins/";
     //Modules directory
     export const _MODULE : string = "modules/";
-    //Regex to catch "name_author/plugins"
-    export const _EXPR_GIT : RegExp = /:(.*)\.git$/;
+    // Regex to catch "name_author/plugins".
+    // This expression is valid both https and ssh address
+    export const _EXPR_GIT : RegExp = /.com[:|\/](.*)\.git$/;
     //Archive code status
     export module Code{
         //Exit with success
         export const _SUCCESS : number = 0;
         //General error
-        export const _GENERAL_ERROR : number = 1
+        export const _GENERAL_ERROR : number = 1;
         //Invalid argument
         export const _INVALID_ARG : number = 128;
     }
